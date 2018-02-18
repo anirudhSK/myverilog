d2d1667a76f9ec38a5d17afc9d7632099337ee15
Anirudh Sivaraman <sk.anirudh@gmail.com>
2018-02-17 22:13:52 -0500

    Take multiplier, increment as input, instead of making them constants.
    This prevents some optimizations like converting x=x*2 to x=x<<1, which seem to reduce the critical path of the multiplier; the effect on the counter is much less pronounced.
    
    Right now, after taking the multiplier and increment as inputs, the multiplier circuit has longer critical path delay than the adder. But still don't know what to do about the unused sequential element error.

--- multiplier.v
+++ multiplier.v
@@ -2,10 +2,11 @@
 `timescale 1ns / 1ps
 
 // module declaration for counter
-module multiplier(clk, multiplier_output);
+module multiplier(clk, multiplier_output, input_multiplier);
 
 // inputs for module
 input  logic  clk;
+input  logic [7:0] input_multiplier;
 
 // outputs for module
 output logic  [7:0] multiplier_output;
@@ -14,6 +15,7 @@ output logic  [7:0] multiplier_output;
 
 // State is anything that's written in an always_ff
 logic  [7:0] multiplier_register = 1;
+logic  [7:0] r_multiplier;
 
 // Wires are transient and are used in always_comb
 logic  [7:0] multiplier_next;
@@ -22,13 +24,14 @@ logic  [7:0] multiplier_next;
 always_comb
 begin
   multiplier_output = multiplier_register;
-  multiplier_next   = multiplier_register * 2;
+  multiplier_next   = multiplier_register * r_multiplier;
 end
 
 // Sequential logic
 always_ff @ (posedge clk)
 begin
   multiplier_register <= multiplier_next;
+  r_multiplier        <= input_multiplier;
 end
 
 // The end
