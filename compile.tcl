1cea7e8aabd1b2897db653c9b72a64964d043b09
Anirudh Sivaraman <sk.anirudh@gmail.com>
2018-02-19 16:29:24 -0500

    Write SDF file as well
    This shows different delays for syn and pnr, but this doesn't show up in the .v files. Must look into this.

--- compile.tcl
+++ compile.tcl
@@ -3,8 +3,10 @@ read_verilog -sv  [lindex $argv 0]
 read_xdc          [lindex $argv 1]
 synth_design -top [lindex $argv 2]
 report_timing
+write_sdf syn_output.sdf
 write_verilog -force syn_output.v
 place_design
 route_design
+write_sdf pnr_output.sdf
 write_verilog -force pnr_output.v
 report_timing
