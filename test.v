197f0ecc0a8ec3b39f2a841a8928980eee4e6fd2
Anirudh Sivaraman <sk.anirudh@gmail.com>
2018-02-19 21:28:50 -0500

    Much wider bit width.
    QoR is still slightly better after PNR relative to before PNR. This doesn't make sense because QoR typically degrades after PNR, but could be explained by the fact that the synthesis STA already assigns some delays to nets that are comparable to---and in some cases less than---the PNR delays for these nets.

--- test.v
+++ test.v
@@ -1,11 +1,11 @@
 // module to take in a test input and write it into a FF
 module test(clk, test_input);
 
-(* dont_touch = "yes" *) input logic clk;
-(* dont_touch = "yes" *) input logic test_input;
-(* dont_touch = "yes" *) logic write_output;
-(* dont_touch = "yes" *) logic read_register;
-(* dont_touch = "yes" *) logic write_register;
+(* dont_touch = "yes" *) input logic       clk;
+(* dont_touch = "yes" *) input logic[15:0] test_input;
+(* dont_touch = "yes" *)       logic[15:0] write_output;
+(* dont_touch = "yes" *)       logic[15:0] read_register;
+(* dont_touch = "yes" *)       logic[15:0] write_register;
 
 always_comb
 begin
