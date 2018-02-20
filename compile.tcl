ed71f34dce4397a4317649dbb40ed5a93850f0fe
Anirudh Sivaraman <sk.anirudh@gmail.com>
2018-02-19 21:04:19 -0500

    compile.tcl now includes SDF delays in syn+pnr timing sim files.
    I think this fixes the issue with the previous commit.

--- compile.tcl
+++ compile.tcl
@@ -3,10 +3,10 @@ read_verilog -sv  [lindex $argv 0]
 read_xdc          [lindex $argv 1]
 synth_design -top [lindex $argv 2]
 report_timing
-write_sdf syn_output.sdf
-write_verilog -force syn_output.v
+write_sdf -force syn_output.sdf
+write_verilog -sdf_anno true -mode timesim -force syn_output.v
 place_design
 route_design
-write_sdf pnr_output.sdf
-write_verilog -force pnr_output.v
+write_sdf -force pnr_output.sdf
+write_verilog -sdf_anno true -mode timesim -force pnr_output.v
 report_timing
--- syn_output.v
+++ syn_output.v
@@ -1,26 +1,24 @@
 // Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2017.3 (lin64) Build 2018833 Wed Oct  4 19:58:07 MDT 2017
-// Date        : Mon Feb 19 15:41:44 2018
+// Date        : Mon Feb 19 21:03:11 2018
 // Host        : anirudh-OptiPlex-7050 running 64-bit Ubuntu 16.04.3 LTS
-// Command     : write_verilog -force syn_output.v
+// Command     : write_verilog -sdf_anno true -mode timesim -force syn_output.v
 // Design      : test
-// Purpose     : This is a Verilog netlist of the current design or from a specific cell of the design. The output is an
-//               IEEE 1364-2001 compliant Verilog HDL file that contains netlist information obtained from the input
-//               design files.
+// Purpose     : This verilog netlist is a timing simulation representation of the design and should not be modified or
+//               synthesized. Please ensure that this netlist is used with the corresponding SDF file.
 // Device      : xc7k70tfbv676-1
 // --------------------------------------------------------------------------------
 `timescale 1 ps / 1 ps
+`define XIL_TIMING
 
-(* STRUCTURAL_NETLIST = "yes" *)
+(* NotValidForBitStream *)
 module test
    (clk,
     test_input);
   (* dont_touch = "yes" *) input clk;
   (* dont_touch = "yes" *) input test_input;
 
-  wire \<const0> ;
-  wire \<const1> ;
   wire clk;
   wire clk_IBUF;
   (* DONT_TOUCH *) wire clk_IBUF_BUFG;
@@ -30,10 +28,9 @@ module test
   (* DONT_TOUCH *) wire write_output;
   (* DONT_TOUCH *) wire write_register;
 
-  GND GND
-       (.G(\<const0> ));
-  VCC VCC
-       (.P(\<const1> ));
+initial begin
+ $sdf_annotate("syn_output.sdf",,,,"tool_control");
+end
   BUFG clk_IBUF_BUFG_inst
        (.I(clk_IBUF),
         .O(clk_IBUF_BUFG));
@@ -45,10 +42,10 @@ module test
   (* KEEP = "yes" *) 
   FDRE read_register_reg
        (.C(clk_IBUF_BUFG),
-        .CE(\<const1> ),
+        .CE(1'b1),
         .D(test_input_IBUF),
         .Q(read_register),
-        .R(\<const0> ));
+        .R(1'b0));
   (* DONT_TOUCH *) 
   IBUF test_input_IBUF_inst
        (.I(test_input),
@@ -62,8 +59,78 @@ module test
   (* KEEP = "yes" *) 
   FDRE write_register_reg
        (.C(clk_IBUF_BUFG),
-        .CE(\<const1> ),
+        .CE(1'b1),
         .D(write_output),
         .Q(write_register),
-        .R(\<const0> ));
+        .R(1'b0));
 endmodule
+`ifndef GLBL
+`define GLBL
+`timescale  1 ps / 1 ps
+
+module glbl ();
+
+    parameter ROC_WIDTH = 100000;
+    parameter TOC_WIDTH = 0;
+
+//--------   STARTUP Globals --------------
+    wire GSR;
+    wire GTS;
+    wire GWE;
+    wire PRLD;
+    tri1 p_up_tmp;
+    tri (weak1, strong0) PLL_LOCKG = p_up_tmp;
+
+    wire PROGB_GLBL;
+    wire CCLKO_GLBL;
+    wire FCSBO_GLBL;
+    wire [3:0] DO_GLBL;
+    wire [3:0] DI_GLBL;
+   
+    reg GSR_int;
+    reg GTS_int;
+    reg PRLD_int;
+
+//--------   JTAG Globals --------------
+    wire JTAG_TDO_GLBL;
+    wire JTAG_TCK_GLBL;
+    wire JTAG_TDI_GLBL;
+    wire JTAG_TMS_GLBL;
+    wire JTAG_TRST_GLBL;
+
+    reg JTAG_CAPTURE_GLBL;
+    reg JTAG_RESET_GLBL;
+    reg JTAG_SHIFT_GLBL;
+    reg JTAG_UPDATE_GLBL;
+    reg JTAG_RUNTEST_GLBL;
+
+    reg JTAG_SEL1_GLBL = 0;
+    reg JTAG_SEL2_GLBL = 0 ;
+    reg JTAG_SEL3_GLBL = 0;
+    reg JTAG_SEL4_GLBL = 0;
+
+    reg JTAG_USER_TDO1_GLBL = 1'bz;
+    reg JTAG_USER_TDO2_GLBL = 1'bz;
+    reg JTAG_USER_TDO3_GLBL = 1'bz;
+    reg JTAG_USER_TDO4_GLBL = 1'bz;
+
+    assign (strong1, weak0) GSR = GSR_int;
+    assign (strong1, weak0) GTS = GTS_int;
+    assign (weak1, weak0) PRLD = PRLD_int;
+
+    initial begin
+	GSR_int = 1'b1;
+	PRLD_int = 1'b1;
+	#(ROC_WIDTH)
+	GSR_int = 1'b0;
+	PRLD_int = 1'b0;
+    end
+
+    initial begin
+	GTS_int = 1'b1;
+	#(TOC_WIDTH)
+	GTS_int = 1'b0;
+    end
+
+endmodule
+`endif
