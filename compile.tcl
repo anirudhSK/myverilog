da517d4fed7fb431028f120a5bf6241f6082e26e
Anirudh Sivaraman <sk.anirudh@gmail.com>
2018-02-20 16:03:33 -0500

    Report both setup and hold time violations

--- compile.tcl
+++ compile.tcl
@@ -2,11 +2,13 @@
 read_verilog -sv  [lindex $argv 0]
 read_xdc          [lindex $argv 1]
 synth_design -top [lindex $argv 2]
-report_timing
+report_timing -setup
+report_timing -hold
 write_sdf -force syn_output.sdf
 write_verilog -sdf_anno true -mode timesim -force syn_output.v
 place_design
 route_design
 write_sdf -force pnr_output.sdf
 write_verilog -sdf_anno true -mode timesim -force pnr_output.v
-report_timing
+report_timing -setup
+report_timing -hold
