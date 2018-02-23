3f593fd82015379de382301c59ccbf7e8f915084
Anirudh Sivaraman <sk.anirudh@gmail.com>
2018-02-23 18:16:23 -0500

    Add part number and timing summary

--- compile.tcl
+++ compile.tcl
@@ -1,4 +1,5 @@
-# Synthesis script
+# Compiler script
+set_part xc7a35tcpg236-1
 read_verilog -sv  [lindex $argv 0]
 read_xdc          [lindex $argv 1]
 synth_design -top [lindex $argv 2]
@@ -12,3 +13,4 @@ write_sdf -force pnr_output.sdf
 write_verilog -sdf_anno true -mode timesim -force pnr_output.v
 report_timing -setup
 report_timing -hold
+report_timing_summary
