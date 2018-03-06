6801f5a5989fd728835ff6a922b593177ca42ac3
Anirudh Sivaraman <sk.anirudh@gmail.com>
2018-03-06 11:44:10 -0500

    Try the runtimeoptimized directive
    Doesn't change synthesis result at all. Run time is roughly the same.

--- compile_lite.tcl
+++ compile_lite.tcl
@@ -2,7 +2,7 @@
 set_part xc7a35tcpg236-1
 read_verilog -sv  [lindex $argv 0]
 read_xdc          [lindex $argv 1]
-synth_design -top [lindex $argv 2]
+synth_design -directive runtimeoptimized -top [lindex $argv 2]
 place_design
 route_design
 report_timing -setup
