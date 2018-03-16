7bfca3a8be540cc808c692c03a5249baf5a26af8
Anirudh Sivaraman <sk.anirudh@gmail.com>
2018-03-16 16:41:59 -0400

    Add version of compile lite script
    Hardcode compile_lite.tcl bc read_verilog doesn't seem to take wildcards
    
    Confirmed that this version toggles the LED every 10 seconds (1B increments at 100 MHz)

---
+++ up_counter/compile_lite.tcl
@@ -0,0 +1,11 @@
+# Lite version of compiler script
+set_part xc7a35tcpg236-1
+read_verilog -sv  counter.sv counter_top.sv led_display.sv
+read_xdc          constraints.xdc
+synth_design -top counter_top
+place_design
+route_design
+report_timing -setup
+report_timing -hold
+report_timing_summary
+write_bitstream -force output.bit
