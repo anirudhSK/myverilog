547513cc8644d09f2cb9f829e73a9ffa1e125f2f
Anirudh Sivaraman <sk.anirudh@gmail.com>
2018-03-16 20:39:36 -0400

    Explicitly set all seven segment related pins to high
    Prevents the seven segment display from being dimly lit, probably because the default value (0) corresponds to enabling the seven segment pins.

--- up_counter/counter_top.sv
+++ up_counter/counter_top.sv
@@ -1,13 +1,30 @@
 module counter_top (
   input  logic clk,
   input  logic i_reset,
-  output logic o_led
+  output logic o_led,
+  output logic [0:6] seg,
+  output logic [0:3] an,
+  output logic dp
 );
 
 // Intermediate wires
 (* dont_touch = "yes" *) logic [0:31] w_out;
 (* dont_touch = "yes" *) logic w_locked;
 (* dont_touch = "yes" *) logic clk_gen;
+
+// Wires to disable seven segment display
+assign seg[0] = 1;
+assign seg[1] = 1;
+assign seg[2] = 1;
+assign seg[3] = 1;
+assign seg[4] = 1;
+assign seg[5] = 1;
+assign seg[6] = 1;
+assign an[0]  = 1;
+assign an[1]  = 1;
+assign an[2]  = 1;
+assign an[3]  = 1;
+assign dp     = 1;
 
 clk_gen(.clk_in(clk),
         .reset(1'b0),
