8c476aaead4bfaa98547e6c7a893c27fdf0838d9
Anirudh Sivaraman <sk.anirudh@gmail.com>
2018-03-19 22:13:32 -0400

    uart_transceiver actually compiles with no errors.

--- up_counter/compile_lite.tcl
+++ up_counter/compile_lite.tcl
@@ -1,11 +1,11 @@
 # Lite version of compiler script
 set_part xc7a35tcpg236-1
-read_verilog -sv  counter.sv counter_top.sv led_display.sv clk_gen.sv
+read_verilog -sv  uart_transmitter.sv
 read_xdc          constraints.xdc
-synth_design -top counter_top
+synth_design -top uart_transmitter
 place_design
 route_design
 report_timing -setup
 report_timing -hold
 report_timing_summary
-write_bitstream -force output.bit
+#write_bitstream -force output.bit
