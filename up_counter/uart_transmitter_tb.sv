5244b3ae92b684887d3df83fa05e09b967fa8e47
Anirudh Sivaraman <sk.anirudh@gmail.com>
2018-03-19 23:54:55 -0400

    Many fixes to testbench; sim output seems sensible
    Most of the errors with the testbench not the original code. Seems good to try out.

--- up_counter/uart_transmitter_tb.sv
+++ up_counter/uart_transmitter_tb.sv
@@ -3,7 +3,7 @@ module uart_testbench ();
 logic clk;
 logic i_reset;
 logic i_start_transmission;
-logic i_data;
+logic [7:0] i_data;
 logic o_tx;
 
 uart_transmitter tx (
@@ -21,16 +21,20 @@ initial begin
   i_start_transmission = 0;
   #10
   i_reset = 1;
+  #2
+  i_reset = 0;
   #100
   i_start_transmission = 1;
-  i_data = 8'b11110000;
+  i_data = 8'b10101010;
+  #2
+  i_start_transmission = 0;
   #4000
   $finish;
 end
 
 initial begin
-  $display("\t\ttime,\tclk,\ti_reset,\ti_start_transmission,\to_tx");
-  $monitor("%d,\t%b,\t%b,\t\t%b,\t\t\t%b", $time, clk, i_reset, i_start_transmission, o_tx);
+  $display("\t\ttime,\tclk,\ti_reset,\ti_start_transmission,\to_tx,\ti_data");
+  $monitor("%d,\t%b,\t%b,\t\t%b,\t\t\t%b,\t%d", $time, clk, i_reset, i_start_transmission, o_tx, i_data);
 end
 
 always
