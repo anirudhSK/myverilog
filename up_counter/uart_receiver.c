1051fb07f7e48ec8dacc831afdcc6440c14c497f
Anirudh Sivaraman <sk.anirudh@gmail.com>
2018-03-21 11:43:54 -0400

    Several minor changes:
    * Change port name to USB1.
    * Change baud rate to 9600.
    * Add timestamp.
    * Rename file.

--- up_counter/uart_receiver.c
+++ up_counter/uart_receiver.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <termios.h>
 #include <unistd.h>
+#include <sys/time.h>
 
 int set_interface_attribs(int fd, int speed)
 {
@@ -60,7 +61,7 @@ void set_mincount(int fd, int mcount)
 
 int main()
 {
-    char *portname = "/dev/ttyUSB0";
+    char *portname = "/dev/ttyUSB1";
     int fd;
     int wlen;
 
@@ -69,8 +70,8 @@ int main()
         printf("Error opening %s: %s\n", portname, strerror(errno));
         return -1;
     }
-    /*baudrate 115200, 8 bits, no parity, 1 stop bit */
-    set_interface_attribs(fd, B115200);
+    /*baudrate 9600, 8 bits, no parity, 1 stop bit */
+    set_interface_attribs(fd, B9600);
     //set_mincount(fd, 0);                /* set to pure timed read */
 
     /* simple output */
@@ -80,7 +81,6 @@ int main()
     }
     tcdrain(fd);    /* delay for output */
 
-
     /* simple noncanonical input */
     do {
         unsigned char buf[80];
@@ -88,12 +88,14 @@ int main()
 
         rdlen = read(fd, buf, sizeof(buf) - 1);
         if (rdlen > 0) {
+            struct timeval now;
+            gettimeofday(&now, NULL);
 #ifdef DISPLAY_STRING
             buf[rdlen] = 0;
-            printf("Read %d: \"%s\"\n", rdlen, buf);
+            printf("Time: %d.%06ld Read %d: \"%s\"\n", (int)now.tv_sec, (long)now.tv_usec, rdlen, buf);
 #else /* display hex */
             unsigned char   *p;
-            printf("Read %d:", rdlen);
+            printf("Time: %d.%06ld Read %d:", (int)now.tv_sec, (long)now.tv_usec, rdlen);
             for (p = buf; rdlen-- > 0; p++)
                 printf(" 0x%x", *p);
             printf("\n");
