21ac29788f407e1748f2b3f791d1462fd704ca35
Anirudh Sivaraman <sk.anirudh@gmail.com>
2018-03-28 15:54:41 -0400

    Some fixes to uart_receiver to be more precise

--- up_counter/uart_receiver.sv
+++ up_counter/uart_receiver.sv
@@ -44,23 +44,24 @@ begin
 
   if (r_current_state == IDLE) begin
     // Look for a low bit
-    w_next_state = (r_rx == 1'b0) ? START_BIT : w_next_state;
+    w_next_state       = (r_rx == 1'b0) ? START_BIT : w_next_state;
+    w_next_cycle_count = 1; // Already saw one low sample.
   end else if (r_current_state == START_BIT) begin
     if (r_rx == 1'b0) begin
-      w_next_state       = (r_current_cycle_count < HALF_BIT) ? START_BIT : DATA_BITS;
-      w_next_cycle_count = (r_current_cycle_count < HALF_BIT) ? r_current_cycle_count + 1 : 0;
+      w_next_state       = (r_current_cycle_count == HALF_BIT) ? DATA_BITS : START_BIT;
+      w_next_cycle_count = (r_current_cycle_count == HALF_BIT) ? 1         : r_current_cycle_count + 1;
     end else begin
       w_next_state       = IDLE;
       w_next_cycle_count = 0;
     end
   end else if (r_current_state == DATA_BITS) begin
-    if (r_current_cycle_count < FULL_BIT) begin
-      w_next_cycle_count = r_current_cycle_count + 1;
-    end else begin
+    if (r_current_cycle_count == FULL_BIT) begin
       r_current_data[r_current_bit] = r_rx; // Sample middle bit
-      w_next_bit   = (r_current_bit < 7) ? r_current_bit + 1 : 0;
-      w_next_state = (r_current_bit < 7) ? w_next_state : STOP_BIT; 
+      w_next_bit   = (r_current_bit == 7) ? 0        : r_current_bit + 1;
+      w_next_state = (r_current_bit == 7) ? STOP_BIT : w_next_state; 
       w_next_cycle_count = 0;
+    end else begin
+      w_next_cycle_count = r_current_cycle_count + 1;
     end
   end else begin // STOP bit
     if (r_current_cycle_count < FULL_BIT) begin
