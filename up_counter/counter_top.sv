762b72a3c43731dba22ac709f6c36003d46ee73b
Anirudh Sivaraman <sk.anirudh@gmail.com>
2018-04-01 00:35:57 -0400

    Remove UART receiver from counter

--- up_counter/counter_top.sv
+++ up_counter/counter_top.sv
@@ -1,13 +1,11 @@
 module counter_top (
   input  logic clk,
   input  logic i_reset,
-  input  logic i_uart_rx,
+  output logic o_uart_tx,
   output logic o_count_led,
-  output logic o_uart_led,
   output logic [0:6] o_segment_enable, // Is a segment (a to g, i.e., 7 segments) enabled (0) or not (1)?
   output logic [0:3] o_display_enable, // Is a particular 7 segment display enabled (0) or not (1)? There are 4 such displays.
-  output logic o_dot_enable,           // Is the dot on the 7 segment display enabled (0) or not (1)?
-  output logic [0:7] o_debug           // For LEDS from received UART data
+  output logic o_dot_enable            // Is the dot on the 7 segment display enabled (0) or not (1)?
 );
 
 // Intermediate wires
@@ -16,8 +14,6 @@ module counter_top (
 (* dont_touch = "yes" *) logic w_start_uart;
 (* dont_touch = "yes" *) logic clk_gen;
 (* dont_touch = "yes" *) logic [0:7] w_uart_tx_data;
-(* dont_touch = "yes" *) logic w_ready_to_read;
-(* dont_touch = "yes" *) logic [0:7] w_uart_rx_data;
 
 // Wires to enable/disable seven segment display
 // Note: 0 is enable. 1 is disable.
@@ -39,42 +35,26 @@ clk_gen(.i_clk_in(clk),
         .o_locked(w_locked),
         .o_clk_out(clk_gen));
 
-// counter counter_instance (
-//   .clk(clk_gen),
-//   .i_reset(i_reset),
-//   .o_count_next(w_out),
-//   .o_start_uart(w_start_uart),
-//   .o_uart_data(w_uart_data)
-// );
-// 
-// led_display led_display_instance (                    
-//   .i_count(w_out),
-//   .clk(clk_gen),
-//   .o_led(o_count_led)
-// );
-// 
-// uart_transmitter uart_tx (
-//   .clk(clk_gen),
-//   .i_reset(i_reset),
-//   .i_data(w_uart_tx_data),
-//   .i_start_transmission(w_start_uart),
-//   .o_tx(o_uart_tx)
-// );
-
-uart_receiver uart_rx (
+counter counter_instance (
   .clk(clk_gen),
   .i_reset(i_reset),
-  .i_rx(i_uart_rx),
-  .o_data(w_uart_rx_data),
-  .o_ready_to_read(w_ready_to_read),
-  .o_debug(o_debug)
+  .o_count_next(w_out),
+  .o_start_uart(w_start_uart),
+  .o_uart_data(w_uart_data)
 );
 
-uart_led_output uart_led_instance (
-  .i_data(w_uart_rx_data),
-  .i_ready_to_read(w_ready_to_read),
+led_display led_display_instance (                    
+  .i_count(w_out),
   .clk(clk_gen),
-  .o_led(o_uart_led)
+  .o_led(o_count_led)
+);
+
+uart_transmitter uart_tx (
+  .clk(clk_gen),
+  .i_reset(i_reset),
+  .i_data(w_uart_tx_data),
+  .i_start_transmission(w_start_uart),
+  .o_tx(o_uart_tx)
 );
 
 endmodule
